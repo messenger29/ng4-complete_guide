cbe5bd5554408e48de9b72560ae5274cb60e1267
John Y <john.j.yu@stanford.edu>
2017-07-12 16:45:39 -0700

    fresh starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,5 @@ import { FormsModule } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '';
+
 }
